2a1ec3040207551f4c5da4c364ae81be4373d881
cosondu1 <cosondu1@myseneca.ca>
2023-09-29 20:19:09 -0400

    added routes

--- blog-service.js
+++ blog-service.js
@@ -1,57 +1,62 @@
-const file = require('fs');     //to use file system module
-var employees = [];
-var departments = [];
+const blogService = require('./blog-service.js');
 
-exports.initialize = () => {
-    return new Promise ((resolve, reject) => {
-        file.readFile('./data/employees.json', (err,data) => {
-            if (err) {
-                reject ('unable to read file');
-            }
-            else {
-                employees = JSON.parse(data);
-            }
+
+const fs = require('fs');
+
+// Globally declared arrays
+let posts = [];
+let categories = [];
+
+// Initialize function
+function initialize() {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./data/posts.json', 'utf8', (err, data) => {
+            if (err) return reject('Unable to read file');
+            posts = JSON.parse(data);
+
+            fs.readFile('./data/categories.json', 'utf8', (err, data) => {
+                if (err) return reject('Unable to read file');
+                categories = JSON.parse(data);
+                resolve();
+            });
         });
+    });
+}
 
-        file.readFile('./data/departments.json', (err,data)=> {
-            if (err) {
-                reject ('unable to read file');
-            }
-            else {
-                departments = JSON.parse(data);
-            }
-        })
-        resolve();
-    })
-};
-exports.getAllEmployees = () => {
-    return new Promise ((resolve,reject) => {
-        if (employees.length == 0) {
-            reject('no results returned');
+function getAllPosts() {
+    return new Promise((resolve, reject) => {
+        if(posts.length > 0) {
+            resolve(posts);
+        } else {
+            reject('No results returned');
         }
-        else {
-            resolve(employees);
-        }
-    })
-};
+    });
+}
 
-exports.getManagers = () => {
-    return new Promise ((resolve, reject) => {
-        var managers = employees.filter(employee => employee.isManager == true);
-        if (managers.length == 0) {
-            reject('no results returned');
+function getPublishedPosts() {
+    return new Promise((resolve, reject) => {
+        const publishedPosts = posts.filter(post => post.published);
+        if(publishedPosts.length > 0) {
+            resolve(publishedPosts);
+        } else {
+            reject('No results returned');
         }
-        resolve(managers);
-    })
-};
+    });
+}
 
-exports.getDepartments = () => {
-    return new Promise((resolve,reject) => {
-        if (departments.length == 0) {
-            reject ('no results returned');
+function getCategories() {
+    return new Promise((resolve, reject) => {
+        if(categories.length > 0) {
+            resolve(categories);
+        } else {
+            reject('No results returned');
         }
-        else {
-            resolve (departments);
-        }
-    })
+    });
+}
+
+module.exports = {
+    initialize,
+    getAllPosts,
+    getPublishedPosts,
+    getCategories
 };
--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Chidera Osondu Student ID: 174098210  Date: Friday 29th Sept
+*
+*  Online (Cyclic) Link: ________________________________________________________
+*
+********************************************************************************/ 
+
 var express = require("express");
 var app = express();
 var path = require('path');
@@ -6,16 +17,59 @@ var HTTP_PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 
+const blogService = require('./blog-service.js');
+
 app.get('/', (req, res) => {
-    // Redirect the user to the "/about" route
-    res.redirect('/about');
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
 
-app.get('/about', (req, res) => {
-    // Return the about.html file from the views folder
-    res.sendFile(path.join(__dirname, "/views/about.html"));
+app.get('/blog', (req, res) => {
+    blogService.getPublishedPosts()
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.json({ message: err });
+        });
 });
 
-app.listen(HTTP_PORT, () => {
-    console.log('Express http server listening on port ' + HTTP_PORT);
+
+app.get('/posts', (req, res) => {
+    blogService.getAllPosts()
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.json({ message: err });
+        });
 });
+
+app.get('/categories', (req, res) => {
+    blogService.getCategories()
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.json({ message: err });
+        });
+});
+
+
+// Catch-all for non-matching routes
+app.use((req, res, next) => {
+    res.status(404).send("Page Not Found");
+});
+
+// app.listen(HTTP_PORT, () => {
+//     console.log('Express http server listening on port ' + HTTP_PORT);
+// });
+
+blogService.initialize()
+    .then(() => {
+        app.listen(8080, () => {
+            console.log('Server is running on http://localhost:8080');
+        });
+    })
+    .catch(err => {
+        console.error('Failed to initialize blog service:', err);
+    });
